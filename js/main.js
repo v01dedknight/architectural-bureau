58ee6585b8c665a7a1914285e3f64cb0bf91787a
v01d <Kevin_Vearez@mail.ru>
2026-03-13 00:36:44 +0700

    ip changed for server

--- js/main.js
+++ js/main.js
@@ -230,7 +230,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     const message = form.message.value.trim();
 
                     try {
-                        const response = await fetch('http://127.0.0.1:8000/send', {
+                        const response = await fetch('/send', {
                             method: 'POST',
                             headers: {'Content-Type': 'application/json'},
                             body: JSON.stringify({ name, phone, email, message })
--- js/portfolio.js
+++ js/portfolio.js
@@ -133,7 +133,7 @@
                         const payload = { name, phone, email, message };
 
                         try {
-                            const response = await fetch('http://127.0.0.1:8000/send', {
+                            const response = await fetch('/send', {
                                 method: 'POST',
                                 headers: {'Content-Type': 'application/json'},
                                 body: JSON.stringify(payload)
